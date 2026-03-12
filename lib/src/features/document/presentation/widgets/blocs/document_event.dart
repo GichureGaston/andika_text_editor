2d1c7d270da5e3a0212b4b097c6665a77decdc2b
Gaston <gichuregaston@gmail.com>
2026-03-12 11:53:38 +0300

    populate event with needed parameters

--- lib/src/features/document/presentation/widgets/blocs/document_event.dart
+++ lib/src/features/document/presentation/widgets/blocs/document_event.dart
@@ -4,13 +4,62 @@ part of 'document_bloc.dart';
 sealed class DocumentEvent extends Equatable {
   const DocumentEvent();
   @override
-  List<Object> get props => [];
+  List<Object?> get props => [];
 }
 
-class CreateDocument extends DocumentEvent {}
+class CreateDocument extends DocumentEvent {
+  final String documentId;
+  final String owner;
+
+  const CreateDocument({required this.documentId, required this.owner});
+
+  @override
+  List<Object?> get props => [documentId, owner];
+}
+
+class LoadDocument extends DocumentEvent {
+  final String documentId;
+
+  const LoadDocument({required this.documentId});
+
+  @override
+  List<Object?> get props => [documentId];
+}
+
+class LoadAllDocuments extends DocumentEvent {
+  final String userId;
+
+  const LoadAllDocuments({required this.userId});
+
+  @override
+  List<Object?> get props => [userId];
+}
+
+class UpdateDocument extends DocumentEvent {
+  final String documentId;
+  final DocumentPageDataModel documentPage;
+
+  const UpdateDocument({required this.documentId, required this.documentPage});
+
+  @override
+  List<Object?> get props => [documentId, documentPage];
+}
+
+class UpdateDocumentDelta extends DocumentEvent {
+  final String pageId;
+  final DeltaData deltaData;
 
 class SaveDocument extends DocumentEvent {}
 
-class CheckDocumentForError extends DocumentEvent {}
+  @override
+  List<Object?> get props => [pageId, deltaData];
+}
 
-class EditDocument extends DocumentEvent {}
+class SubscribeToDocument extends DocumentEvent {
+  final String pageId;
+
+  const SubscribeToDocument({required this.pageId});
+
+  @override
+  List<Object?> get props => [pageId];
+}
